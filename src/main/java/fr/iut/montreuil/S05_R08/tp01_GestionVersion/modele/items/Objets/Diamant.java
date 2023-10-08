eeddef142605e5e10b91af21962ae276e7cdf2f3
Hugo <101125294+Lony027@users.noreply.github.com>
2023-10-08 15:52:42 +0200

    modif anglais support

--- src/main/java/fr/iut/montreuil/S05_R08/tp01_GestionVersion/modele/items/Objets/Diamant.java
+++ src/main/java/fr/iut/montreuil/S05_R08/tp01_GestionVersion/modele/items/Objets/Diamant.java
@@ -3,6 +3,7 @@ package fr.iut.montreuil.S05_R08.tp01_GestionVersion.modele.items.Objets;
 public class Diamant extends Objet{
 
     public Diamant () {
+        
         super("Diamant", 200);
     }
 }
